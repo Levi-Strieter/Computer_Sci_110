3d8002a188b2eb8e4fa9dd4d7f907e6f023c419a
Strieter <levi.strieter03@outlook.com>
2021-09-09 11:20:57 -0400

    tests

---
+++ Homework/HW_Template.py
@@ -0,0 +1,19 @@
+import math
+
+print("Problem R1.20")
+'''  (three single quotes in a row signal the start of a multi-line comment)
+Insert your pseudo-code here. It will not be executed by the Python interpreter.
+
+'''
+
+print("Problem P1.11")
+#insert code here
+
+print("Problem P2.3")
+#insert code here
+
+print("Problem P2.8")
+#insert code here
+
+print("Problem P2.11")
+#insert code here
